3af63d29328e433b714c451f6a3c407f67cb9434
caiog19 <caio@ejcm.com.br>
2021-10-30 17:29:09 -0300

    Telas - Presentation, Introduction

--- front/App.tsx
+++ front/App.tsx
@@ -3,10 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Presentation from './src/screens/Presentation';
 import Introduction from './src/screens/Introduction';
+import { useFonts } from 'expo-font';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  
+    const [ loaded ] = useFonts({
+      Capriola: require('./assets/Capriola-Regular.ttf'),
+      
+    })
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
---
+++ front/src/screens/Introduction/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+export const CatsImage = styled.Image`
+    width: ${wp('100%')};
+    height: ${hp('45%')};
+`
+export const LogoSmallerImage = styled.Image`
+    width: 104.34px;
+    height: 62.16px;
+`
