af4f93ffb267126e5dc8adf0b1bc12b5de8debf1
guimatera <guilhermematera@ejcm.com.br>
2021-10-31 16:38:56 -0300

    multer

--- back/src/app.js
+++ back/src/app.js
@@ -1,16 +1,19 @@
-const express = require('express');
 require('./config/dotenv')();
 require('./config/sequelize');
 
+const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const path = require('path');
 const cors = require('cors');
 const routes = require('./routes/routes');
 
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 
 app.get('/', (req, res) => {
