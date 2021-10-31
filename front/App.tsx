2c45c86b8bcf866bec17149026c355ab382e7c96
caiog19 <caio@ejcm.com.br>
2021-10-30 21:39:22 -0300

    paginas cadastro

--- front/App.tsx
+++ front/App.tsx
@@ -4,8 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Presentation from './src/screens/Presentation';
 import Introduction from './src/screens/Introduction';
 import Login from './src/screens/Login';
-
+import Register from './src/screens/Register';
+import Register2 from './src/screens/Register2'
 import { useFonts } from 'expo-font';
+import PurpleButton from './src/components/PurpleButton';
 
 const Stack = createStackNavigator();
 
@@ -21,10 +23,11 @@ export default function App() {
           headerShown: false
          }}>
            
-    
     <Stack.Screen name = "Presentation" component={Presentation}/>
     <Stack.Screen name = "Introduction" component={Introduction}/>
     <Stack.Screen name = "Login" component = {Login}/>
+    <Stack.Screen name = "Register" component = {Register}/>
+    <Stack.Screen name = "Register2" component = {Register2}/>      
     
       </Stack.Navigator>
 
